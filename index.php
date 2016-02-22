2fa6f2c5c07d047d7afc47d8348aead912386b5c
dadewoyin <dadewoyin16@gmail.com>
2016-02-22 04:05:57 -0500

    Removal

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php 
 
-$head = '<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title>MCMT</title>
-<meta name="description" content="MCMT is a brand founded by Maria Cecilia Mendoza-Tinoco.">
-<meta name="keywords" content="MCMT art painting paint Maria Cecilia Mendoza Tinoco creative nice paintings">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="style.css" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css"/>
-<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.5.9/slick-theme.css"/>
-</head>';
+
 
 ?>
